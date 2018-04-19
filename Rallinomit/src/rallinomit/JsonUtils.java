c103d8c794536df06edd9b1afa43a3635b8ddec6
Rautiainen Aleksi <s1601392@student.businesscollege.fi>
2018-04-19 13:32:59 +0300

    PaneUtilsiin on toteutettu vaihdaIkkuna-metodi, jolla voidaan vaihtaa käyttöliittymän ikkunoita. Lisätty JsonUtils luokka.

---
+++ Rallinomit/src/rallinomit/JsonUtils.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rallinomit;
+
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Aleksi Rautiainen
+ */
+public class JsonUtils {
+
+    // Tämän luokan tarkoitus on luoda kansio, tallentaa ja lukea Json-tiedostosta.
+    public static void luoKansio() {
+
+    }
+
+    public void lueTiedostosta() {
+
+    }
+
+    public void tallennaTiedostoon(ObservableList<Rank> tulokset) {
+
+    }
+}
